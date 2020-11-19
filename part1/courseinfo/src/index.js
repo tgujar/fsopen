18dab141aff35a715aa5669403fd270c3c4634e4
Tanmay Gujar <tanmaygujar999@gmail.com>
2020-11-19 19:10:34 +0530

    completed 1.3, 1.4 and 1.5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -19,26 +19,28 @@ const Content = (props) => {
 
 const Total = (props) => {
   return <p>
-  Number of exercises {props.exercises.reduce((tot, num) => {
-    return Number(num) + tot;
+  Number of exercises {props.parts.reduce((tot, num) => {
+    return Number(num.exercises) + tot;
   }, 0)}</p>
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const course = {
+  name: 'Half Stack application development',
+  parts: [
     {name: 'Fundamentals of React', exercises: 10},
     {name: 'Using props to pass data', exercises: 7},
     {name: 'State of a component', exercises: 14},
-  ];
+  ]};
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total exercises={parts.map(part => part.exercises)} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
